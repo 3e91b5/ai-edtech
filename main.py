4bc4249c09c0a253924b72f398df856d74a1b683
jean-jsj <151345238+jean-jsj@users.noreply.github.com>
2023-11-18 22:39:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -75,9 +75,9 @@ class EdTechApp:
 				)
 		if app == "Home":
 			with st.form("form1"):
-	    		sid = st.text_input('ID:', autocomplete="on", placeholder="아이디 입력", max_chars=10)
-			spwd = st.text_input('Password:', type='password', placeholder="4자리 비밀번호 입력", max_chars=4)
-	    		submitted = st.form_submit_button("로그인")
+	    			sid = st.text_input('ID:', autocomplete="on", placeholder="아이디 입력", max_chars=10)
+				spwd = st.text_input('Password:', type='password', placeholder="4자리 비밀번호 입력", max_chars=4)
+	    			submitted = st.form_submit_button("로그인")
 			df = run_query('SELECT * FROM student ORDER BY sid ')
 			st.table(df)
 		if app == 'Problem-sets':
