6b358fe9aea2b55845be669f088e758219e5847a
jean-jsj <151345238+jean-jsj@users.noreply.github.com>
2023-11-18 20:50:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
 import streamlit as st
+
+st.set_page_config(
+		page_title = "AI-EdTech",
+)
+
+from streamlit_option_menu import option_menu
+import problemset, account
+
+class MultiApp:
+	def __init__(self):
+		self.apps = [] 
+	def add_app(self, title, function):
+		self.apps.append({
+			"title": title,
+			"function": function
+		})
+	
+	def run():
+		with st.sidebar:
+			app = option_menu(
+			menu_title = 'Menu',
+			options=['Main', 'Problem-sets', 'Account'],
+			icons=['house-fill', 'trophy-fill', 'person-circle'],
+			menu_icon='chat-text-fill',
+			default_index=1,
+			styles={
+				"container": {"padding": "5!important", "background-color": 'black'},
+				"icon": {"color": "white", "font-size": "23px"},
+				"nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "0px"},
+				"nav-link-selected": {"background-color": "#02ab21"},}
+				)
+    # run the app function inside the main.py file
+		if app == 'Problem-sets':
+			problemset.app() 
+		if app == 'Account':
+			account.app() 
+	
+	run()
