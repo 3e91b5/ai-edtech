69eb45ce15506014d8d4f37747fcfe6e1b9a839e
3e91b5 <jaehunkim@snu.ac.kr>
2023-11-28 00:45:30 +0900

    change sid into student_id in src/db.py

--- src/db.py
+++ src/db.py
@@ -46,55 +46,51 @@ def get_student_info(account):
 	else:
 		return result
 
-def add_user(sid,password, name, age, grade):
-	# query = f"SELECT * FROM edutech.studentdb WHERE sid = '{sid}'"
-	query = f"SELECT * FROM student_db.students WHERE account = '{sid}'"
+# 
+def add_user(student_id, password, name, age, grade):
+	query = f"SELECT * FROM student_db.students WHERE account = '{student_id}'"
 	print(datetime.datetime.now(), "check query:", query)
 	result = run_query(query)
 
 	if result.empty:
-		print(datetime.datetime.now(), "user data를 추가합니다.", sid, password)
-		# add_query = f"INSERT INTO edutech.studentdb (sid, password, admin) VALUES('{sid}', '{password}', False)"
+		print(datetime.datetime.now(), "user data를 추가합니다.", student_id, password)
 		date_joined = datetime.datetime.now()
-		add_query = f"INSERT INTO student_db.students (account, password, admin, name,age, grade,date_joined) VALUES('{sid}', '{password}', False,'{name}', '{age}', '{grade}', '{date_joined}')"
+		add_query = f"INSERT INTO student_db.students (account, password, admin, name,age, grade,date_joined) VALUES('{student_id}', '{password}', False,'{name}', '{age}', '{grade}', '{date_joined}')"
 
 		run_tx(add_query)
 		return True
 	else:
 		return False
 
-def login_user(sid,password):
-	# query =f"SELECT * FROM edutech.studentdb WHERE sid ='{sid}' AND password = '{password}'"
-	query = f"SELECT * FROM student_db.students WHERE account = '{sid}' AND password = '{password}'"
+def login_user(student_id,password):
+	query = f"SELECT * FROM student_db.students WHERE account = '{student_id}' AND password = '{password}'"
 	result = run_query(query)
 	if result.empty:
 		return False
 	else:
 		last_login = datetime.datetime.now()
-		query = f"UPDATE student_db.students SET last_login = '{last_login}' WHERE account = '{sid}'"
+		query = f"UPDATE student_db.students SET last_login = '{last_login}' WHERE account = '{student_id}'"
 		run_tx(query)
 		return True
 
 def view_all_users():
-	# query = 'SELECT * FROM edutech.studentdb'
 	query = 'SELECT * FROM student_db.students'
 	result = run_query(query)
 	return result
 
-def delete_user(sid):
-	query = f"select * from student_db.students where account = '{sid}'"
+def delete_user(student_id):
+	query = f"select * from student_db.students where account = '{student_id}'"
 	result = run_query(query)
 	if result.empty:
 		return False
-	query = f"DELETE FROM student_db.students WHERE account = '{sid}'"
+	query = f"DELETE FROM student_db.students WHERE account = '{student_id}'"
 	run_tx(query)
-	print(datetime.datetime.now(), "delete user", sid)
+	print(datetime.datetime.now(), "delete user", student_id)
 	
 	return True
 
-def is_admin(sid):
-	# query = f"SELECT admin FROM edutech.studentdb WHERE sid = '{sid}'"
-	query = f"SELECT admin FROM student_db.students WHERE account = '{sid}'"
+def is_admin(student_id):
+	query = f"SELECT admin FROM student_db.students WHERE account = '{student_id}'"
 	result = run_query(query)
 	print(datetime.datetime.now(), "is_admin",  result['admin'][0])
 	
@@ -103,48 +99,48 @@ def is_admin(sid):
 	else:
 		return False
 
-def update_user_password(sid, new_password):
-	query = f"UPDATE student_db.students SET password = '{new_password}' WHERE account = '{sid}'"
+def update_user_password(student_id, new_password):
+	query = f"UPDATE student_db.students SET password = '{new_password}' WHERE account = '{student_id}'"
 	result = run_tx(query)
 	if result.empty:
 		return False
 	else:
 		return True
 	
-def update_student_score(sid, score):
+def update_student_score(student_id, score):
 	return
 	#TODO: after db schema is fixed, implement this function. below is example code.		
-	query = f"UPDATE edutech.studentdb SET score = '{score}' WHERE sid = '{sid}'"
+	query = f"UPDATE edutech.studentdb SET score = '{score}' WHERE student_id = '{student_id}'"
 	result = run_tx(query)
 	if result.empty:
 		return False
 	else:
 		return True
 
-def get_student_score(sid):
+def get_student_score(student_id):
 	return
 	#TODO: after db schema is fixed, implement this function. below is example code.
-	query = f"SELECT score FROM edutech.studentdb WHERE sid = '{sid}'"
+	query = f"SELECT score FROM edutech.studentdb WHERE student_id = '{student_id}'"
 	result = run_query(query)
 	if result.empty:
 		return False
 	else:
 		return result
 
-def get_student_status(sid):
+def get_student_status(student_id):
 	return
 	#TODO: after db schema is fixed, implement this function. below is example code.
-	query = f"SELECT status FROM edutech.studentdb WHERE sid = '{sid}'"
+	query = f"SELECT status FROM edutech.studentdb WHERE student_id = '{student_id}'"
 	result = run_query(query)
 	if result.empty:
 		return False
 	else:
 		return result
 
-def get_student_level(sid):
+def get_student_level(student_id):
 	return
 	#TODO: after db schema is fixed, implement this function. below is example code.
-	query = f"SELECT level FROM edutech.studentdb WHERE sid = '{sid}'"
+	query = f"SELECT level FROM edutech.studentdb WHERE student_id = '{student_id}'"
 	result = run_query(query)
 	if result.empty:
 		return False
@@ -163,7 +159,7 @@ def get_problems_recommendation(level, score):
 		return result
 
 # given the level of student and the score vector from student's answer, get one problem from database
-def get_problem_recommendation(level, score, sid):
+def get_problem_recommendation(level, score, student_id):
 	return
 	#TODO: after db schema is fixed, implement this function. below is example code.
 	query = f"SELECT * FROM edutech.problems WHERE level = '{level}' AND score = '{score}'"
